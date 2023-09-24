dc06bd1305ba5463087cd839355d634341ccbea2
PosionTM <tommylol134@gmail.com>
2023-09-23 23:43:10 -0400

    Rough draft of basic UI setup

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import ConversationWindow from "./components/ConversationWindow";
 
 const App = () => {
     return (
-        <div id="container">
-            Hello world!
+        <div className="container">
+            <div className="header">
+                <div className="burger_icon">|||</div>
+                    <div className="sub_header">
+                        <div id="main_title">Cartoon Chat Hub</div>
+                    </div>
+            </div>
+            <div className="people_window"> People Window </div>
+            < ConversationWindow />
+            <div className="input_window">Input Window</div>
         </div>
     )
 }
---
+++ Client/src/components/ConversationWindow.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ConversationWindow = () => {
+
+    const fetchConversation = () => {
+        console.log("test");
+    }
+
+    return (
+        <div className="conversation_window">
+            Conversation Window
+        </div>
+    )
+}
+
+export default ConversationWindow;
